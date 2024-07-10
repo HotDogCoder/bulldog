938f1301e758aab44b0292e375168cab201d3e60
Jorge Hospinal <jorgehospinalflores@gmail.com>
2024-07-10 00:36:28 -0500

    fix

--- apps/peru/application/services/sunat_service.py
+++ apps/peru/application/services/sunat_service.py
@@ -40,7 +40,7 @@ class SunatService(SunatServiceInterface):
                 chrome_options.add_argument("--proxy-server='direct://'")
                 chrome_options.add_argument("--proxy-bypass-list=*")
                 chrome_options.add_argument("--start-maximized")
-                chrome_options.add_argument('--headless')
+                # chrome_options.add_argument('--headless')
                 chrome_options.add_argument('--disable-gpu')
                 chrome_options.add_argument('--disable-dev-shm-usage')
                 chrome_options.add_argument('--no-sandbox')
